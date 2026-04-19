864aa0c977e6c7554a525969d8fe3890aad09477
Surjya Bhowmick <bhowmickneeds@gmail.com>
2026-04-20 01:03:26 +0600

    added DDA line algorithm

--- solar_system.py
+++ solar_system.py
@@ -17,6 +17,27 @@ white = (255, 255, 255)
 def plot_pixel(x, y, color):
     screen.set_at((int(x), int(y)), color)
 
+def dda_line(x1, y1, x2, y2, color):
+    dx = x2 - x1
+    dy = y2 - y1
+
+    steps = max(abs(dx), abs(dy))
+
+    if steps == 0:
+        plot_pixel(x1, y1, color)
+        return
+
+    x_increment = dx / steps
+    y_increment = dy / steps
+
+    x = x1
+    y = y1
+
+    for i in range(int(steps) + 1):
+        plot_pixel(round(x), round(y), color)
+        x += x_increment
+        y += y_increment
+
 running = True
 while running:
     screen.fill(black)
@@ -31,8 +52,8 @@ while running:
     # Planet
     pygame.draw.circle(screen, blue, (550, 300), 15)
 
-    # One white pixel
-    plot_pixel(100, 100, white)
+    # DDA line
+    dda_line(100, 100, 300, 200, white)
 
     pygame.display.update()
 
