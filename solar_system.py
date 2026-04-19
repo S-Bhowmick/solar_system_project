7e91eab399d5d1aa5b668f78dc076a0be50f955c
Surjya Bhowmick <bhowmickneeds@gmail.com>
2026-04-20 00:58:52 +0600

    added sun and one planet

--- solar_system.py
+++ solar_system.py
@@ -10,6 +10,8 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Solar System Project")
 
 black = (0, 0, 0)
+yellow = (255, 255, 0)
+blue = (0, 100, 255)
 
 running = True
 while running:
@@ -18,6 +20,12 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # Sun
+    pygame.draw.circle(screen, yellow, (400, 300), 40)
+
+    # Planet
+    pygame.draw.circle(screen, blue, (550, 300), 15)
 
     pygame.display.update()
 
